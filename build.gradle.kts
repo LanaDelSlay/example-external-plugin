6106a21c5abc47fa9b3522918fbaee5b57570a07
LanaDelSlay <gmiller1902@gmail.com>
2020-05-30 22:19:44 -0700

    1

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,14 +8,14 @@ plugins {
     checkstyle
 }
 
-project.extra["GithubUrl"] = "http://github.com/<INSERT NAME>/<INSERT REPOSITORY>"
+project.extra["GithubUrl"] = "https://github.com/LanaDelSlay/tests"
 
 apply<BootstrapPlugin>()
 
 subprojects {
     group = "com.example"
 
-    project.extra["PluginProvider"] = ""
+    project.extra["PluginProvider"] = "LanaDelSlay"
     project.extra["ProjectSupportUrl"] = ""
     project.extra["PluginLicense"] = "3-Clause BSD License"
 
